f60d42c737955974831dbf6cde4ea5bf6febd745
AhmedNabil1994 <94ahmed.nabil@gmail.com>
2024-11-07 19:01:41 +0200

    nav + header

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// ------------------------------------home typing animation----------------------------
+(function typingAnimation() {
+  let options = {
+    strings: ["Larry Daniels", "Developer", "Designer"],
+    typeSpeed: 60,
+    backSpeed: 60,
+    backDelay: 700,
+    loop: true,
+  };
+  new Typed(".typing", options);
+})();
+
