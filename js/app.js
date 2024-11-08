a743766a1fccba68aa19ed4df5faf33c2c056fa3
AhmedNabil1994 <94ahmed.nabil@gmail.com>
2024-11-08 15:45:20 +0200

    portfolio html slider

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
+// ------------------------------------global elements variable----------------------------
+// nav elements
+const barsIcon = document.querySelector(".fa-bars");
+const navbar = document.getElementById("navbar");
+const mobileList = document.getElementById("navbarSupportedContent");
+const navLinks = document.querySelectorAll("li.nav-item.link");
+// slider
+const portfolioSlider = document.querySelector(".slider");
+const sliderImages = document.querySelectorAll("#portfolioModal img");
+// nav and tabs zoom icons in portfolio
+const allZoomIcons = document.querySelectorAll("#pills-all .fa-search-plus");
+console.log(allZoomIcons);
+
 // ------------------------------------home typing animation----------------------------
 (function typingAnimation() {
   let options = {
@@ -10,3 +23,47 @@
   new Typed(".typing", options);
 })();
 
+// ------------------------------------portfolio sliders effect----------------------------
+// general function of portfolio tabs images
+function tabsImagesEffect(zoomIcons, sliderImages) {
+  zoomIcons.forEach(tabTypeCallback);
+  function tabTypeCallback(zoomIcon) {
+    sliderImages.forEach(sliderCallback);
+    function sliderCallback(sliderImage) {
+      zoomIcon.addEventListener("click", () => {
+        // test here
+        if (tabTypeImage.src === sliderImage.src) {
+          sliderImage.parentElement.parentElement.classList.add("active");
+        }
+      });
+    }
+  }
+}
+
+// clicking portfolio image to show slider
+(function portfolioImagesEffect() {
+  tabsImagesEffect(allZoomIcons, sliderImages);
+})();
+
+// remove active class when closing the slider
+function removeActiveClass(sliderImages) {
+  sliderImages.forEach((sliderImage) => {
+    sliderImage.parentElement.parentElement.classList.remove("active");
+  });
+}
+
+// closing slider effect
+(function sliderCloseEffect() {
+  allSliders.forEach(sliderCallback);
+  function sliderCallback(slider) {
+    slider.addEventListener("transitionend", () => {
+      if (!slider.classList.contains("show")) {
+        removeActiveClass(sliderOneImages);
+        removeActiveClass(sliderTwoImages);
+        removeActiveClass(sliderThreeImages);
+        removeActiveClass(sliderFourImages);
+        removeActiveClass(sliderFiveImages);
+      }
+    });
+  }
+})();
